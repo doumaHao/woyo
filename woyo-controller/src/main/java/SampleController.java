717780146323bc822c91ecdc7491477c167f7ef0
HaoXijun <809103454@qq.com>
2017-11-11 11:17:30 +0800

    Initial commit

---
+++ woyo-controller/src/main/java/SampleController.java
@@ -0,0 +1,27 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Copyright (c) 2017. douma.cn, All Rights Reserved
+ * Project Name : woyo
+ * Date : 2017/11/10 17:31
+ * Author : douma
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+
+}
